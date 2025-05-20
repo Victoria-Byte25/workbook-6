cefc03ea4dd165e93685a5a4f70e14bfa4900da9
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-05-20 13:12:41 -0500

    Add WorkingWithInterfaces project implementing Comparable

--- WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        List<Person> myFamily = new ArrayList<>();
+
+        myFamily.add(new Person("Dave", "Dykes", 56));
+        myFamily.add(new Person("Jane", "Westly", 31));
+        myFamily.add(new Person("Melchi", "Komi", 5));
+        myFamily.add(new Person("Karyna", "Krupodor", 26));
+        myFamily.add(new Person("Victoria", "Brown", 25));
+        myFamily.add(new Person("Mo", "Sudan", 17));
+        myFamily.add(new Person("Lola", "Nade", 12)); // to test age sort
+
+        System.out.println("Before sorting:");
+        myFamily.forEach(System.out::println);
+        Collections.sort(myFamily);
+
+        System.out.println("\nAfter sorting by lastName > firstName > age:");
+        myFamily.forEach(System.out::println);
+    }
 }
+
--- WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getAge() { return age; }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (Age " + age + ")";
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        // First: compare by last name
+        int lastCompare = this.lastName.compareTo(other.lastName);
+
+        if (lastCompare == 0) {
+            int firstCompare = this.firstName.compareTo(other.firstName);
+
+            if (firstCompare == 0) {
+                return Integer.compare(this.age, other.age);
+            }
+            return firstCompare;
+        }
+
+        return lastCompare;
+    }
 }
