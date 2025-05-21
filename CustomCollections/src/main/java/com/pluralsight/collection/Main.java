7d07aaf289376ded1025d0abc5f5dadc58ae28db
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-05-21 11:15:34 -0500

    initial commit

--- CustomCollections/src/main/java/com/pluralsight/collection/Main.java
+++ CustomCollections/src/main/java/com/pluralsight/collection/Main.java
@@ -1,4 +1,28 @@
 package com.pluralsight.collection;
 
-public class Main {
+import java.time.LocalDate;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        FixedList<Integer> numbers = new FixedList<>(3);
+        numbers.add(10);
+        numbers.add(3);
+        numbers.add(92);
+        numbers.add(43);
+
+        System.out.println("Numbers size: " + numbers.getCurrentSize()); // 3
+
+        FixedList<LocalDate> dates = new FixedList<>(2);
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.of(2024, 1, 1)); // should fail
+
+        System.out.println("Dates size: " + dates.getCurrentSize()); // 2
+
+        FixedList<String> names = new FixedList<>(2);
+        names.add("Naade");
+        names.add("Victoria");
+        names.add("Babalola");
+    }
 }
